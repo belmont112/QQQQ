eb7a04bfe46d46047b5940dc4bb441848d03ffe5
justinoh711 <justinoh711@gmail.com>
2019-09-19 19:45:56 -0400

    2nd comment

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author justi
+ */
+public class test {
+    private String name;
+    private int no;
+
+    public String getName(){
+        return name;
+
+    }
+}
